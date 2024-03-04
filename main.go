9005fb1a8e9a62b8b04cc29776c43b43593812a9
mrbraden56 <bradenlock83@gmail.com>
2024-03-03 21:27:27 -0600

    adding prints

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("This is a deployed project using Go, Docker, Github, and Homebrew!")
+	fmt.Println("This is a deployed project using Go, Docker, Github, and Homebrew! Editing a little bit...")
 	Honey()
 }
